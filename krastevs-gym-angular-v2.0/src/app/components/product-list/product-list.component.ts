08491d2cc1e7bb3bd277bdd133a9752bacec546f
beinsaduno <krastevtodordinev@yahoo.com>
2023-11-02 16:01:58 +0100

    Working on Products by Category

--- krastevs-gym-angular-v2.0/src/app/components/product-list/product-list.component.ts
+++ krastevs-gym-angular-v2.0/src/app/components/product-list/product-list.component.ts
@@ -5,7 +5,7 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-grid.component.html',
+  templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
