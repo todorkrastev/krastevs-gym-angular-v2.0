a413c1c1df5d1eb8a8f7c40c0a038baf4dd9ae9b
todorkrastev <krastevtodordinev@yahoo.com>
2023-11-23 03:11:16 +0100

    Add Footer Component

--- krastevs-gym-angular-v2.0/src/app/app.component.ts
+++ krastevs-gym-angular-v2.0/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './navigation/header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
