32913a5dae96706b5bd5bed68bb31c8f88e44d2c
todorkrastev <krastevtodordinev@yahoo.com>
2023-11-23 02:32:47 +0100

    Add Header Component

--- krastevs-gym-angular-v2.0/src/app/app.component.ts
+++ krastevs-gym-angular-v2.0/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
